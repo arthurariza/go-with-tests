228b566c4a39dc5283e3fb281de33888be4e5123
arthurariza <tucaariza@gmail.com>
2024-01-20 02:44:58 -0300

    Iteration

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module tdd
 
-go 1.21.4
+go 1.21.6
---
+++ iteration/repeat_test.go
@@ -0,0 +1,36 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeatsFiveTimes(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func TestRepeatsFourTimes(t *testing.T) {
+	repeated := Repeat("a", 4)
+	expected := "aaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 3)
+	}
+}
+
+func ExampleRepeat() {
+	repeat := Repeat("A", 3)
+	fmt.Println(repeat)
+	// Output: AAA
+}
